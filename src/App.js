43eba2527b2645c039e026abf44bcf0cdf54aaf7
Mohammad-Harkous <mohammad.a.harkous@gmail.com>
2022-12-18 14:12:43 +0200

    Weather API: HTML/CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import mostlycloudy from './images/weather-icons/mostlycloudy.svg';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <nav>
+      <form>
+      <input type="text" name="wethear" placeholder="Type in a city name"/>
+      <button> FIND WEATHER</button>
+      </form>
+    </nav>
+    <main>
+      
+      <div className="wethear_now">
+        <img src={mostlycloudy} alt="clear"/>
+      </div>
+
+      
+    </main>
     </div>
   );
 }
