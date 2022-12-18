f3bf2d431d9c24046cbe738e9289b0837278b2c1
fatema <fatemaalbatoulfakhourji@gmail.com>
2022-12-18 19:57:32 +0200

    Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import mostlycloudy from './images/weather-icons/mostlycloudy.svg';
+import clear from './images/weather-icons/clear.svg';
 
 
 function App() {
@@ -7,17 +8,47 @@ function App() {
     <div className="App">
     <nav>
       <form>
-      <input type="text" name="wethear" placeholder="Type in a city name"/>
-      <button> FIND WEATHER</button>
+      <input type="text" name="wethear" placeholder="london"/>
+      <button className="mar"> FIND WEATHER</button>
       </form>
     </nav>
     <main>
       
       <div className="wethear_now">
-        <img src={mostlycloudy} alt="clear"/>
+        <img className="image" src={mostlycloudy} alt="mostly cloudy" />
+        <p className='color'> <strong>overcast cloud</strong></p>
+        <p><strong>Temperature</strong> &nbsp;&nbsp;10<span>&#176;</span> to 11<span>&#176;</span>C</p>
+        <p className='size'><strong>Humidity</strong> &nbsp;&nbsp;78<span>&#37;</span>&nbsp;&nbsp;<strong>Pressure</strong>&nbsp;&nbsp;1008.48</p>
       </div>
-
-      
+      <div className='days'>
+         <div className='info'>
+          <p>03:00</p>
+          <p>09:00</p>
+          <p>09:00</p>
+          <p>12:00</p>
+          <p>15:00</p>
+          <p>18:00</p>
+          <p>21:00</p>
+         </div>
+         <div className='info'>
+         <img src={mostlycloudy} alt="mostly cloudy" />
+         <img src={mostlycloudy} alt="mostly cloudy" />
+         <img src={clear} alt="clear" />
+         <img src={clear} alt="clear" />
+         <img src={clear} alt="clear" />
+         <img src={clear} alt="clear" />
+         <img src={mostlycloudy} alt="mostly cloudy" />
+         </div>
+         <div className='info'>
+          <p>8&#176;C</p>
+          <p>9&#176;C</p>
+          <p>14&#176;C</p>
+          <p>17&#176;C</p>
+          <p>18&#176;C</p>
+          <p>16&#176;C</p>
+          <p>13&#176;C</p>
+         </div>
+      </div>
     </main>
     </div>
   );
